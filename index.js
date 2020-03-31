f220232127945513dcda35a3a3ea6469b51ea0ad
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-31 03:12:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function updateObjectWithKeyAndValue (obj, key, value) {
   return Object.assign({},obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
+  obj[key] = value
+
+  return obj
+}

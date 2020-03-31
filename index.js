5b5bc9316dd6512894e0cf2d2c95c427148d5d6a
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-31 03:18:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
 }
 
 function deleteFromObjectByKey (object, key) {
-  delete object[key]
+  delete object.key
   return object
 }
